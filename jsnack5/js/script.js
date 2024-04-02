5aaf3c99b42883f3ee8f8aa70f1473dafdc1d2b6
lucasens1 <lucasensini1@gmail.com>
2024-04-02 16:33:11 +0200

    fix

--- jsnack5/js/script.js
+++ jsnack5/js/script.js
@@ -3,7 +3,7 @@ const userNumbers = [];
 for(let i = 0; i < 6; i++){
     let userN = parseInt(prompt("Inserisci un numero :"));
     
-    if(userN % 2 === 0){
+    if(userN % 2 !== 0){
         userNumbers[i] = userN;
     }
 }
